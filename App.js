b829d36597182d3dad6022e8ebc787c1a6c9e643
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-02 13:05:34 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+const parent = React.createElement(
+    "div", 
+    {id:"grandparent", xyz:"abc"}, 
+    [
+        React.createElement(
+            "div", 
+            {id:"parent1"},
+            [
+                React.createElement("h1",{id:"child"},"I am child1 of parent1"),
+                React.createElement("h1",{id:"child"},"I am child2 of parent1")
+            ]
+         ),
+         React.createElement(
+            "div", 
+            {id:"parent2"},
+            [
+                React.createElement("h1",{id:"child"},"I am child1 of parent2"),
+                React.createElement("h1",{id:"child"},"I am child2 of parent2")
+            ]
+         )
+        ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
