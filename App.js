b666879a5e57d80dce6d9e90facb08626df7b7aa
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-04 22:49:15 +0530

    component composition

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = (
+// React Functional Component
+const Title = () => (
 <h1 className="head" tabIndex="5">
-    Namaste React using JSX</h1>
+   I am component in component</h1>
     );
-    console.log(jsxHeading);
 
 
 // React Functional Component
+// Component Composition
 const HeadingComponent = () => (
     <div>
+        <Title/>
         <h1 className="heading"> Namaste React Functional Component1</h1>
         <h1 className="heading2"> Namaste React Functional Component2</h1>
     </div>
 )
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent/>) 
