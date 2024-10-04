6c49850a8b1e9050e311cd6aa51597e89829fa06
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-04 22:22:28 +0530

    use jsx

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    {id:"grandparent", xyz:"abc"}, 
-    [
-        React.createElement(
-            "div", 
-            {id:"parent1"},
-            [
-                React.createElement("h1",{id:"child"},"I am child1 of parent1"),
-                React.createElement("h1",{id:"child"},"I am child2 of parent1")
-            ]
-         ),
-         React.createElement(
-            "div", 
-            {id:"parent2"},
-            [
-                React.createElement("h1",{id:"child"},"I am child1 of parent2"),
-                React.createElement("h1",{id:"child"},"I am child2 of parent2")
-            ]
-         )
-        ]
-);
+// JSX (transipied before it reacher the JS) - PARCEL -Babel
+// JSX => Babel transpile it to React.createElement => ReactElement-JS Object =>HTMLElement(render)
+
+const jsxHeading = (
+<h1 className="head" tabIndex="5">
+    Namaste React using JSX</h1>
+    );
+    console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(jsxHeading) 
