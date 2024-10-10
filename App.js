d0cc38a031f57759107a6492d36bc714647b7657
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-10 22:23:38 +0530

    refactor

--- App.js
+++ App.js
@@ -2,29 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // jsx or relact element
-const elem = <span>React Element ,</span>
+const elem = <span>React Element ,</span>;
 
 // usingreact component
-const Title =  ()=>  (
-<h1 className="head" tabIndex="5">
-    {elem}
-   I am component in component</h1>
-    );
-
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    {elem}I am component in component
+  </h1>
+);
 
 // React Functional Component
 // using react component in three different ways
-const HeadingComponent = () => ( 
-    <div>
-        {Title()}
-        <Title/>
-        <Title></Title>
-        <h1 className="heading"> Namaste React Functional Component1</h1>
-        <h1 className="heading2"> Namaste React Functional Component2</h1>
-    </div>
-)
-
-
+const HeadingComponent = () => (
+  <div>
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading"> Namaste React Functional Component1</h1>
+    <h1 className="heading2"> Namaste React Functional Component2</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>) 
+root.render(<HeadingComponent />);
