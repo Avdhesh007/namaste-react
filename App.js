1109e865db784c63e0c842e9046b159bcb150838
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-14 22:42:22 +0530

    added one card

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import ReactDOM from "react-dom/client";
  * - Search
  * - RestaurantContainer
  *  - Restaurant card
+ *    - Img
+ *    - Name of res, start rating, cuisine, delivery time.
  * Footer
  *  - CopyRight
  *  - Links
@@ -37,16 +39,41 @@ const Header = () => {
   );
 };
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+      />
+      <h3> Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
-      {" "}
-      {
-        // Header
-        <Header />
-        //Body
-        // Footer
-      }
+      <Header></Header>
+      <Body></Body>
     </div>
   );
 };
