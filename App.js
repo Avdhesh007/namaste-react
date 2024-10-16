70f112e0acf4fe541ea587c85b2f9e91f8e88d7d
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-16 22:29:06 +0530

    props destructuring

--- App.js
+++ App.js
@@ -58,8 +58,7 @@ const Body = () => {
 const styleCard = {
   backgroundColor: "#f0f0f0",
 };
-const RestaurantCard = (props) => {
-  console.log(props);
+const RestaurantCard = ({ resName, cuisine }) => {
   return (
     <div className="res-card" style={styleCard}>
       <img
@@ -67,8 +66,8 @@ const RestaurantCard = (props) => {
         alt="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
       />
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
     </div>
