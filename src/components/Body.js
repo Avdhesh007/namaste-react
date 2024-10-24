0bda92355496ef2265bc447550c68141e7b31db1
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-24 21:51:42 +0530

    onClick handler

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,16 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search"> Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("Clicked");
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
         {resData.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
