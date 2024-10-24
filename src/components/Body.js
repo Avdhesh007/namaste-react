f6661a3659276a9f3691e2e0f0837c6e1300b1d4
Avdhesh Kumar <avdhesh0076@gmail.com>
2024-10-24 22:04:51 +0530

    without hook, filter not working

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,20 +2,25 @@ import resData from "../MockData/data.json";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  let listOfRestaurants = resData;
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Clicked");
+            // Filter logic here
+            listOfRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.6
+            );
+            console.log(listOfRestaurants);
           }}
         >
           Top Rated Restaurant
         </button>
       </div>
       <div className="res-container">
-        {resData.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
         ))}
       </div>
