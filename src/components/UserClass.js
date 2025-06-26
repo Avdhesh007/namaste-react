bf6c0c5ed4c9a44feb85dc1d5a63bd0503119a88
Avdhesh Kumar <avdhesh0076@gmail.com>
2025-06-27 00:17:35 +0530

    variables in classBased component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,6 +3,10 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     return (
@@ -10,6 +14,16 @@ class UserClass extends React.Component {
         <h2> Name: {this.props.name}</h2>
         <h3>Location: Pune</h3>
         <h4>Contact: avdhesh0076@gmail.com</h4>
+        <h4>Count: {this.state.count}</h4>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count+
+        </button>
       </div>
     );
   }
