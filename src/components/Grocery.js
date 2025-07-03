23d897fcd522a222ac63b6727765cee9d1f13445
Avdhesh Kumar <avdhesh0076@gmail.com>
2025-07-03 23:21:05 +0530

    added tailwind

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,5 +1,5 @@
 const Grocery = () => {
-  return <h1> We are in Grocery </h1>;
+  return <h1 class="text-3xl font-bold underline"> We are in Grocery </h1>;
 };
 
 export default Grocery;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,21 +1,18 @@
 import { CDN_URL } from "../utils/constants";
 
-const styleCard = {
-  backgroundColor: "#f0f0f0",
-};
 const RestaurantCard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, cuisines, avgRating, sla, costForTwo } =
     resData;
   return (
-    <div className="res-card" style={styleCard}>
+    <div className="res-card bg-gray-200 border-black hover:bg-gray-300 size-80 h-full">
       <img
-        className="res-logo"
+        className="res-logo rounded-lg"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
       />
 
-      <h3>{name}</h3>
+      <h3 className="font-bold text-lg">{name}</h3>
       <h6>{cuisines.join(",")}</h6>
       <h4>{avgRating}</h4>
       <h4>{sla.slaString}</h4>
